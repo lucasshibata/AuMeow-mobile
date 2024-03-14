c20cb6b9ea7eae707ec0c8959a44c360f1094482
Lucas Pimentel Shibata <lucaspish@gmail.com>
2024-03-13 22:31:57 -0300

    commit de atualização da main para verificação de email e senha e criação da página de cadastro

--- app/src/main/java/com/example/aumeow/MainActivity.kt
+++ app/src/main/java/com/example/aumeow/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.aumeow
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
@@ -15,10 +18,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btn_entrar = findViewById<Button>(R.id.btn_entrar)
+        val input_email = findViewById<EditText>(R.id.input_email)
+        val input_senha = findViewById<EditText>(R.id.input_senha)
 
         btn_entrar.setOnClickListener {
-            val navegar_principal = Intent(this, PaginaDeNavegacao::class.java)
-            startActivity(navegar_principal)
+            try{
+                //criar banco de dados
+                val bancoDados = openOrCreateDatabase("DB_AUMEOW", MODE_PRIVATE, null)
+
+                //criar tabela
+                bancoDados.execSQL("CREATE TABLE IF NOT EXISTS TB_USUARIOS(email VARCHAR, senha VARCHAR)")
+
+                //inserir dados em uma tabela
+                //bancoDados.execSQL("INSERT INTO TB_USUARIOS VALUES ('teste@gmail.com', '6789')")
+
+                val consulta = "SELECT email, senha FROM TB_USUARIOS"
+
+                val cursor = bancoDados.rawQuery(consulta, null)
+
+                //recuperar os indices da nossa tabela
+                val indiceEmail = cursor.getColumnIndex("email")
+                val indiceSenha = cursor.getColumnIndex("senha")
+                cursor.moveToFirst()
+
+                while (cursor != null){
+                    val email = cursor.getString(indiceEmail)
+                    val senha = cursor.getString(indiceSenha)
+                    if (email == input_email.text.toString() && senha == input_senha.text.toString()){
+                        Log.i("Resultado", "tá dando certo")
+                        val navegar_principal = Intent(this, PaginaDeNavegacao::class.java)
+                        startActivity(navegar_principal)
+                    }
+                    Log.i("Resultado", "emai: $email | senha: $senha")
+                    cursor.moveToNext()
+                }
+
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+
         }
     }
 }
---
+++ app/src/main/java/com/example/aumeow/PaginaDeCadastro.kt
@@ -0,0 +1,11 @@
+package com.example.aumeow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PaginaDeCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagina_de_cadastro)
+    }
+}
