b017704866db336844d0dab366ffc4d1a0e80582
Lucas Pimentel Shibata <lucaspish@gmail.com>
2024-04-27 13:40:32 -0300

    pagina de vacinação e página de hospedagem modificadas

--- app/src/main/java/com/example/aumeow/PaginaDeFeedback.kt
+++ app/src/main/java/com/example/aumeow/PaginaDeFeedback.kt
@@ -2,10 +2,17 @@ package com.example.aumeow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.core.content.ContextCompat
 
 class PaginaDeFeedback : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagina_de_feedback)
+
+        val sugestao = findViewById<EditText>(R.id.caixa_sugestao)
+
+        sugestao.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.cor_borda_botão))
+
     }
 }
