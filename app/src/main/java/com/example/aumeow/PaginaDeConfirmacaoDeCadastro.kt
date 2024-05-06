7b387adba7bce76b081bd0c4917edab66b8e4bd5
Lucas Pimentel Shibata <lucaspish@gmail.com>
2024-05-06 10:54:02 -0300

    criação de uma página para confirmação

---
+++ app/src/main/java/com/example/aumeow/PaginaDeConfirmacaoDeCadastro.kt
@@ -0,0 +1,20 @@
+package com.example.aumeow
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PaginaDeConfirmacaoDeCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pagina_de_confirmacao_de_cadastro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
