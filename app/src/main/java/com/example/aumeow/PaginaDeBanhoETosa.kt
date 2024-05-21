e4049058b1d1fc9c958cd0b545062291c96af97f
Lucas Pimentel Shibata <lucaspish@gmail.com>
2024-05-20 21:34:17 -0300

    tentativas de mudança na agenda, nada pronto ainda

--- app/src/main/java/com/example/aumeow/PaginaDeBanhoETosa.kt
+++ app/src/main/java/com/example/aumeow/PaginaDeBanhoETosa.kt
@@ -3,18 +3,35 @@ package com.example.aumeow
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Adapter
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aumeow.databinding.ActivityPaginaBanhoEtosaBinding
 
 class PaginaDeBanhoETosa : AppCompatActivity() {
+    lateinit var binding: ActivityPaginaBanhoEtosaBinding
+    var arraylist = ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pagina_banho_etosa)
+        binding = ActivityPaginaBanhoEtosaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btn_voltar = findViewById<ImageView>(R.id.voltar_img_5)
-
-        btn_voltar.setOnClickListener {
+        binding.voltarImg5.setOnClickListener {
             val ir_para_navegacao = Intent(this, PaginaDeNavegacao::class.java)
             startActivity(ir_para_navegacao)
         }
+
+        binding.listaLojas.layoutManager = LinearLayoutManager(this)
     }
 }
+
+//class Adapter (val arrayList: ArrayList<String>):RecyclerView<Adapter.ViewHolder>{
+//    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+//
+//    }
+//}
